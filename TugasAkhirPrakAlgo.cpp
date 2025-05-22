05299332bfcfacc1af8eaeb84ae3771b2dd5d87f
melpaw <melanibunga23@gmail.com>
2025-05-22 18:13:15 +0700

    Add files via upload

---
+++ TugasAkhirPrakAlgo.cpp
@@ -0,0 +1,27 @@
+//Networked Parking System
+#include <iostream>
+#include <string.h> 
+#include <fstream>
+
+using namespace std;
+
+struct Tanggal{
+    int tanggal;
+    int bulan;
+    int tahun;
+};
+
+struct parkir {
+    char plat[20];
+    char jenis[10]; 
+    int jamMasuk;
+    int jamKeluar;
+    int menitMasuk;
+    int menitKeluar;
+    int durasiJam;
+    int biaya;
+    Tanggal tgl;
+    parkir* next; 
+};
+
+struct parkir* head = NULL;
